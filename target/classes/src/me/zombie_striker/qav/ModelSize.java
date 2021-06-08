739ae022b8ab35788f972edc6c9326a9e49ff937
Lorenzo <69311874+Lorenzo0111@users.noreply.github.com>
2021-06-08 16:12:52 +0200

    Updated gitignore

--- target/classes/src/me/zombie_striker/qav/ModelSize.java
+++
@@ -1,7 +0,0 @@
-package me.zombie_striker.qav;
-
-public enum ModelSize {
-
-	BABY_ARMORSTAND_HEAD,ADULT_ARMORSTAND_HEAD,ADULT_ARMORSTAND_HAND;
-
-}
--- target/classes/src/me/zombie_striker/qav/easygui/EasyGUICallable.java
+++
@@ -1,7 +0,0 @@
-package me.zombie_striker.qav.easygui;
-
-public abstract class EasyGUICallable  {
-
-	public abstract void call(ClickData data);
-
-}
