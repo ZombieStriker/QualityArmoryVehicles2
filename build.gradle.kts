b3335d61dcb18c66e6f64471552e7e6c8a1a6821
Lorenzo <69311874+Lorenzo0111@users.noreply.github.com>
2022-02-04 10:54:08 +0100

    Bump version to 2.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "me.zombie_striker"
-version = "2.1.2-SNAPSHOT"
+version = "2.2-SNAPSHOT"
 description = "QualityArmoryVehicles"
 
 java.sourceCompatibility = JavaVersion.VERSION_1_8
